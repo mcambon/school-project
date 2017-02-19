366c648a07bd8fe81aeaa7b39a5d64958d4dae38
Rémi Loze <remi.loze@gmail.com>
2017-02-19 19:23:52 +0100

    Refactor School

--- include/ecole/ecole.h
+++ include/ecole/ecole.h
@@ -1,61 +1,45 @@
 #ifndef ECOLE_H
 #define ECOLE_H
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <ctdio>
+#include <cstdlib>
 
-#include "classe.h"
+#include <string>
+#include <list>
 
+#include "ecole/classe.h"
 
-typedef struct Ecole
+class School 
 {
-		char nomEcole[30];
-		int nombreClasse;
-		Classe_t classes[8];
-}Ecole_t;
 
-//Myriam//
+public:
+	
+	// Constructor
+	School(const std::string& name);
+	
+	// Destructor
+	~School();
+	
+	// Get names of all school_classes
+	std::list<const std::string&> class_names() const;
+	
+	// Get a school class identified by name
+	const SchoolClass& get_class(const std::string& name) const;
+	SchoolClass& get_class(const std::string& name);
+	
+	// Add a school class identified by name
+	void add_class(const std::string& name);
+	
+	// Remove the school class identified by name
+	void remove_class(const std::string& name);
+	
+private:
 
-//////////////////Getters //////////////////
-
-Classe_t * getClasse (Ecole_t * p, int i);
-
-char * getNomEcole (Ecole_t * p);
-
-int getNbClasseEcole (Ecole_t * p);
+	string school_name;
+	std::list<SchoolClass> school_classes;
+}
 
 
-//////////////////Setters ///////////////////
-
-void setNomEcole (Ecole_t * p, char * ch);
-
-
-int setNbClasseEcole (Ecole_t * p, int i);
-
-
-//////////////////////////////////////////////////
-
-/*Fonction appelée au lancement du programme
-* Initialise la structure de l'Ecole et renvoit un pointeur vers cette structure
-* la remplie avec un nom prédéfinit et crée les 8 classes de base
-* les remplira avec la fonction lectureFichier de Ludwig */
-Ecole_t * creerEcole (char * nomEcole, int nbClasse);
-
-/*Permet la saisie d'une nouvelle fiche élève et l'affecte à une classe en fonction de son age
- * Prend un pointeur d'école en entrée et renvoie un pointeur vers l'élève*/
-Eleve_t * nouvelEleve (Ecole_t * p_ecole);
-
-/*Fonction qui affiche la liste de tous les élèves de l'école passée en paramètre d'entrée
- * Elle affiche la liste des élèves de chaque classe de l'école*/
-void afficheEcole (Ecole_t * p_ecole);
-
-/*Fonction qui demande à l'utilisateur un nom et qui cherche dans toutes les classes de l'école si l'élève apparaît.
- * Ne fonctionne pas. Nous n'avons pas eu le temps de la finir*/
-void rechercheDansEcole (Ecole_t * p_ecole);
-
-/*Fonction qui demande à l'utilisateur un nom et une classe et qui cherche dans la classes donnée si l'élève apparaît.
- * Ne fonctionne pas complètement. Elle ne trouve pas le premier élève d'une classe*/
-void recherche (Ecole_t * p_ecole);
 
 #endif
 
--- source/ecole/ecole.c
+++ source/ecole/ecole.c
@@ -1,155 +1,72 @@
 #include "ecole.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <exception>
+#include <algorithm>
+#include <functional>
 
+School::School(const string& name):
+school_name(name)
+{}
 
-void recherche (Ecole_t * p_ecole)
+School::~School()
 {
-	printf("\n\n\n\n\n\n\n1_PS\n2_MS\n3_GS\n4_CP\n5_CE1\n6_CE2\n7_CM1\n8_CM2\n");
-	printf("Dans quelle classe voulez vous chercher?\n");
-	
-	Classe_t * p_classe;
-	Classe_t classe1;
-	p_classe = &classe1;
-	
-	int choix;
-	
-	do
+}
+
+const SchoolClass& School::get_class(const std::string& name) const
+{
+	auto same_name = [name](const SchoolClass& school_class)
 	{
-		printf("Votre choix : ");
-		scanf("%d", &choix);
+		return school_class.get_class_name() == name;
+	};
+	
+	auto it = std::find_if (school_classes.begin(), 
+							school_classes.end(), 
+							same_name);
+							
+	if (it == school_classes.end())
+		return nullptr;
+	
+	return *it;
+}
+
+SchoolClass& School::get_class(const std::string& name)
+{
+	const School* school = const_cast<const School*>(this);
+	const SchoolClass& school_class = school->get_class(name);
+	return const_cast<SchoolClass&>(school_class);
+}
+
+std::list<const std::string&> School::class_names() const
+{
+	std::list<const std::string&> name_list;
+
+	for (const SchoolClass& school_class : school_classes)
+	{
+		name_list.push_back(school_class.get_class_name());
 	}
-	while (choix < 1 || choix > 8);
 	
-	switch (choix)
+	return name_list;
+}
+	
+void School::add_class(const std::string& name)
+{	
+	for (const SchoolClass& school_class : school_classes)
 	{
-	case 1 : p_classe = getClasse(p_ecole, 0);break;
-	case 2 : p_classe = getClasse(p_ecole, 1);break;
-	case 3 : p_classe = getClasse(p_ecole, 2);break;
-	case 4 : p_classe = getClasse(p_ecole, 3);break;
-	case 5 : p_classe = getClasse(p_ecole, 4);break;
-	case 6 : p_classe = getClasse(p_ecole, 5);break;
-	case 7 : p_classe = getClasse(p_ecole, 6);break;
-	case 8 : p_classe = getClasse(p_ecole, 7);break;
-	default : printf("Entrée invalide\n");
+		if (school_class.get_class_name() == name)
+		{
+			throw new std::invalid_argument("A class with the same name exists");
+		}
 	}
-	char nom [50];
-	printf("Tapez le nom à rechercher\n");
-	scanf("%s", nom); //faire un toLower pour récupérer un nom en minuscule
-	rechercheEleve(p_classe, nom);
-		
+	
+	school_classes.emplace_back(name);
 }
 
-void rechercheDansEcole (Ecole_t * p_ecole)
+void School::remove_class(const std::string& name)
 {
-	Classe_t * p_classe;
-	Classe_t classe1;
-	p_classe = &classe1;
-	
-	int nbClasse = getNbClasseEcole(p_ecole);
-	
-	char nom [50];
-	printf("Tapez le nom à rechercher\n");
-	scanf("%s", nom); //Il aurait fallut faire un toLower pour être sur de récupérer un nom en minuscule
-	
-	
-	int i;
-	int test = 1;
-	for (i = 0 ; i < nbClasse ; i++)
+	auto same_name = [name](const SchoolClass& school_class)
 	{
-		p_classe = getClasse(p_ecole, i);
+		return school_class.get_class_name() == name;
+	};
 	
-		do
-		{
-			test = rechercheEleve(p_classe, nom);
-			i++;
-		}
-		while (test != 0 && i < nbClasse);
-		if (test == 1)
-		{
-			afficherEleve(getEleve(p_classe, i)); 
-		}
-		else 
-		{
-		//printf("Il n'y a pas d'èleve %s dans la classe de %s\n", nom, getNomClasse(p_classe));
-		}
-	}			
+	school_classes.remove_if(same_name);
 }
-
-
-Eleve_t * nouvelEleve (Ecole_t * p_ecole)
-{
-	Eleve_t * p_eleve;
-	Eleve_t eleve1;
-	p_eleve = &eleve1;
-	
-	p_eleve = saisieEleve(p_eleve);
-	
-	int niveau = calculNiveau(p_eleve);
-	
-	ajouterEleve(getClasse(p_ecole, niveau), p_eleve);
-	
-	afficheMenuEleve(p_ecole);
-
-return p_eleve;
-}
-
-Classe_t * getClasse (Ecole_t * p, int i)
-{
-	
-return &(p -> classes[i]);
-	
-}
-
-void setNomEcole (Ecole_t * p, char * ch)
-{
-
-	strcpy (p -> nomEcole , ch);
-}
-
-int setNbClasseEcole (Ecole_t * p, int i)
-{
-	p -> nombreClasse = i;
-	return 1;
-}
-
-char * getNomEcole (Ecole_t * p)
-{
-	return p -> nomEcole;
-}
-
-int getNbClasseEcole (Ecole_t * p)
-{
-	return p -> nombreClasse;
-}
-
-
-void afficheEcole (Ecole_t * p_ecole)
-{
-	Classe_t * p_classe;
-	int i;
-	
-	printf("----%s----\n", getNomEcole(p_ecole));
-	printf("Nombre de classes : %d\n", getNbClasseEcole(p_ecole));
-	
-	for (i = 0; i< (getNbClasseEcole(p_ecole)); i++)
-	{
-		p_classe = getClasse (p_ecole, i);
-		afficherClasse(p_classe);
-	}
-}
-
-Ecole_t * creerEcole (char * nomEcole, int nbClasse)
-{
-	Ecole_t * p_ecole;
-	p_ecole = (Ecole_t*) malloc (sizeof(Ecole_t));
-	
-	setNomEcole(p_ecole, nomEcole);
-	setNbClasseEcole(p_ecole, nbClasse);
-
-	return p_ecole;	
-}
-
-
